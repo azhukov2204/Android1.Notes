a177a0f55a541055632ebb78071b8166f1fd7b0e
azhukov2204 <andrew284@mail.ri>
2021-04-12 23:49:31 +0500

    added side menu

--- app/src/main/java/ru/androidlearning/notes/MainActivity.java
+++ app/src/main/java/ru/androidlearning/notes/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.androidlearning.notes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -11,13 +15,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNoteTitlesFragment(savedInstanceState);
+        removeUnnecessaryNoteDetailFragment(); //при смене ориентации на портретную надо удалить фрагмент из noteDetailFragmentContainer, иначе в ToolBar останется его меню
+        initView();
+    }
 
+    private void createNoteTitlesFragment(Bundle savedInstanceState) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         if (savedInstanceState == null) {
             NoteTitlesFragment noteTitlesFragment = new NoteTitlesFragment();
@@ -27,10 +38,6 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
         fragmentManager.popBackStack(NoteTitlesFragment.TITLES_LIST_BACKSTACK_NAME, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-
-        removeUnnecessaryNoteDetailFragment(); //при смене ориентации на портретную надо удалить фрагмент из noteDetailFragmentContainer, иначе в ToolBar останется его меню
-
-        initToolbar();
     }
 
     private void removeUnnecessaryNoteDetailFragment() {
@@ -44,22 +51,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void initToolbar() {
+    private void initView() {
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
+    }
+
+    private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        return toolbar;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Здесь определяем меню приложения (активити)
-        getMenuInflater().inflate(R.menu.content_main_menu, menu);
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Обработка навигационного меню
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (navigateFragment(id)){
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private boolean navigateFragment(int id) {
+
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Обработка выбора пункта меню приложения (активити)
-        return super.onOptionsItemSelected(item);
     }
 
 }
